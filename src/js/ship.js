07e9bbc452f032b7c20a56f88d630571561ed7fb
Clikuki <73450123+clikuki@users.noreply.github.com>
2022-01-21 11:03:15 +0800

    added ship

--- src/js/ship.js
+++ src/js/ship.js
@@ -1,14 +1,69 @@
-// const shipTypes = {
+const SHIPTYPES = {
+	CARRIER: 'CARRIER',
+	BATTLESHIP: 'BATTLESHIP',
+	DESTROYER: 'DESTROYER',
+	SUBMARINE: 'SUBMARINE',
+	PATROLBOAT: 'PATROLBOAT',
+}
 
-// }
+const shipLengths = {
+	CARRIER: 5,
+	BATTLESHIP: 4,
+	DESTROYER: 3,
+	SUBMARINE: 3,
+	PATROLBOAT: 2,
+}
 
-const getNewShip = (type) =>
+// Use function to take advantage of 'this' keyword
+function hit(index)
 {
+	if (!this.indices.includes(index)) return;
+	this.hitIndices.push(index);
+	if (checkIfSunk(this)) this.isSunk = true;
+}
+
+const checkIfSunk = (ship) =>
+{
+	const sameLen = ship.indices.length === ship.hitIndices.length;
+	if (!sameLen) return false;
+	const hasAllVals = ship.hitIndices.every(val => ship.indices.includes(val));
+	return hasAllVals;
+}
+
+const getIndices = (type, startIndex, isVertical) =>
+{
+	const indices = [];
+	const length = shipLengths[type];
+
+	for (let i = 0; i < length; i++)
+	{
+		const index = startIndex + ((isVertical ? 10 : 1) * i);
+		indices.push(index);
+	}
+
+	return indices;
+};
+
+const getNewShip = (type, startIndex, isVertical) =>
+{
+	if (!SHIPTYPES[type]) return null;
+
+	const length = shipLengths[type];
+	const indices = getIndices(type, startIndex, isVertical);
+	const hitIndices = [];
+
 	return {
 		type,
+		length,
+		isVertical,
+		isSunk: false,
+		indices,
+		hitIndices,
+		hit,
 	}
 }
 
 module.exports = {
 	getNewShip,
-}
+	SHIPTYPES
+};
--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,6 +1,69 @@
-const { getNewShip } = require('../js/ship');
+const { getNewShip, SHIPTYPES } = require('../js/ship');
 
-test('test runs', () =>
+let ship;
+beforeEach(() =>
 {
-	expect(true).toBeTruthy()
+	ship = getNewShip(SHIPTYPES.CARRIER, 0, false);
+});
+
+describe('ship properties', () =>
+{
+	test('returns an object', () =>
+	{
+		expect(typeof ship).toBe('object');
+		expect(Array.isArray(ship)).toBeFalsy();
+	})
+
+	test('has indices array', () =>
+	{
+		expect(Array.isArray(ship.indices)).toBeTruthy();
+	})
+
+	test('indices array has correct indices', () =>
+	{
+		const sort = (arr) => arr.sort((a, b) => a - b);
+
+		// Test horizontal
+		const expectedHorizontalIndices = [0, 1, 2, 3, 4];
+		expect(sort(ship.indices)).toEqual(expectedHorizontalIndices);
+
+		// Test vertical
+		ship = getNewShip(SHIPTYPES.CARRIER, 5, true);
+		const expectedVerticalIndices = [5, 15, 25, 35, 45];
+		expect(sort(ship.indices)).toEqual(expectedVerticalIndices);
+	})
+
+	test('fails when no type is passed in', () =>
+	{
+		expect(getNewShip()).toBeNull();
+	})
+})
+
+describe('ship methods', () =>
+{
+	test('hit() correctly sets hit index', () =>
+	{
+		ship.hit(0);
+		expect(ship.hitIndices.includes(0)).toBeTruthy();
+		ship.hit(3);
+		expect(ship.hitIndices.includes(3)).toBeTruthy();
+	})
+
+	test('hit() does not add indices outside of the ship', () =>
+	{
+		ship.hit(6);
+		expect(ship.hitIndices.includes(6)).toBeFalsy();
+	})
+
+	test('hit() sets sunk prop if all indices are hit', () =>
+	{
+		expect(ship.isSunk).toBeFalsy();
+
+		for (let i = 0; i < 5; i++)
+		{
+			ship.hit(i);
+		}
+
+		expect(ship.isSunk).toBeTruthy();
+	})
 })
